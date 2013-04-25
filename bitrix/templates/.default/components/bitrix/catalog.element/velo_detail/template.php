c7412178cecfc65f2e42a4c922ec6e407041838b
Pavel <desbiger@gmail.com>
2013-04-25 17:45:48 +0400

    вывод размеров и цветов в детальной странице товара

--- bitrix/templates/.default/components/bitrix/catalog.element/velo_detail/template.php
+++ bitrix/templates/.default/components/bitrix/catalog.element/velo_detail/template.php
@@ -3,10 +3,14 @@
 } ?>
 <!--<pre>--><?//print_r($arResult)?><!--</pre>-->
 <?
+$array = array();
 CModule::IncludeModule('my_module');
 foreach ($arResult['OFFERS'] as $offer) {
 	foreach ($offer['DISPLAY_PROPERTIES']['CML2_ATTRIBUTES']['VALUE'] as $key => $atr) {
-		$array[$offer['ID']] .= $offer['DISPLAY_PROPERTIES']['CML2_ATTRIBUTES']['DESCRIPTION'][$key] . " : " . $atr . " ";
+		$array[$offer['ID']][] = $atr . " ";
+		$colors[]              = $atr[1];
+		$size[]                = $atr[0];
+		//		$offer['DISPLAY_PROPERTIES']['CML2_ATTRIBUTES']['DESCRIPTION'][$key] . " : " .
 	}
 	//        $colors[$offer['ID']] = $offer['DISPLAY_PROPERTIES']['CML2_ATTRIBUTES']['VALUE'][1];
 	//        $radius[$offer['ID']] = $offer['DISPLAY_PROPERTIES']['CML2_ATTRIBUTES']['VALUE'][0];
@@ -17,8 +21,8 @@ $pics = CFile::ResizeImageGet($arResult['DETAIL_PICTURE']['ID'], array(
 ));
 
 ?>
-<!--    --><?//= count($arResult['OFFERS'])?>
-<!--<pre>--><?//print_r($array)?><!--</pre>-->
+<!--    --><? //= count($arResult['OFFERS'])?>
+<pre><?print_r($array)?></pre>
 
 <div class = "main_picture">
 	<div class = "big_picture">
@@ -38,74 +42,49 @@ $pics = CFile::ResizeImageGet($arResult['DETAIL_PICTURE']['ID'], array(
 	<? endif?>
 </div>
 
-<!--<div class = "opisanie">-->
-<!--	<h4></h4>-->
-<!---->
-<!--	<div class = "price1">17 900 руб</div>-->
-<!---->
-<!--	<form method = "post" action = "#">-->
-<!--		<input type = "submit" name = "text" class = "sub2" value = "В корзину"/>-->
-<!---->
-<!--		<div class = "clear"></div>-->
-<!--		<p>В наличии</p>-->
-<!---->
-<!--		<p>подбор по росту: <input type = "text" id = "growth" class = "text-form"/></p>-->
-<!---->
-<!--		<a href = "#" id = "btn_calc_frame_size">подобрать</a>-->
-<!---->
-<!--		<div id = "frame_size_content" class = "m10"></div>-->
-<!---->
-<!--		<p><b>цвет:</b>-->
-<!--			--><?//=Form::select('offer', $array, true)?>
-<!--		</p>-->
-<!---->
-<!--	</form>-->
-<!---->
-<!--</div>-->
 
-<div class="opisanie">
-    <h4><?=$arResult['NAME']?></h4>
+<div class = "opisanie">
+	<h4><?=$arResult['NAME']?></h4>
 
-    <div class = "price1">17 900 руб</div>
+	<div class = "price1">17 900 руб</div>
 
-    <form method="post" action="#">
-        <input type="submit" name="text" class="sub2" value="в корзину"/>
+	<form method = "post" action = "#">
+		<input type = "submit" name = "text" class = "sub2" value = "в корзину"/>
 
-        <div class="clear"></div>
-        <p>В наличии</p>
+		<div class = "clear"></div>
+		<p>В наличии</p>
 
-        <p>Подбор по росту: <input type="text" id="growth" class="text-form"/>
-            <a href="#" id="btn_calc_frame_size">подобрать</a>
-        </p>
+		<p>Подбор по росту: <input type = "text" id = "growth" class = "text-form"/>
+			<a href = "#" id = "btn_calc_frame_size">подобрать</a>
+		</p>
 
 
-        <div id="frame_size_content" class="m10"></div>
-
-        <div class="select_main">
-            <span>Выберите размер:</span>
-            <ul class="select_size">
-                <li><a href="#">17</a></li>
-                <li><a href="#">18</a></li>
-                <li><a href="#">19</a></li>
-                <li><a href="#">20</a></li>
-                <li><a href="#">21</a></li>
-            </ul>
-        </div>
-        <div class="clear"></div>
-
-        <div class="select_main">
-            <span>Выберите цвет:</span>
-            <ul class="setting_color">
-                <li><a href="#">зелено-синий</a></li>
-                <li><a href="#">зелено-синий</a></li>
-                <li><a href="#">зелено-синий</a></li>
-                <li><a href="#">зелено-синий</a></li>
-                <li><a href="#">зелено-синий</a></li>
-            </ul>
-        </div>
+		<div id = "frame_size_content" class = "m10"></div>
 
 
-    </form>
+		<?if ((bool)$array): ?>
+			<div class = "select_main">
+				<span>Выберите размер:</span>
+				<ul class = "select_size">
+					<?foreach ($array as $size): ?>
+						<li><a href = "#"><?=$size[0]?></a></li>
+					<? endforeach?>
+				</ul>
+			</div>
+			<div class = "clear"></div>
+
+			<div class = "select_main">
+				<span>Выберите цвет:</span>
+				<ul class = "setting_color">
+					<?foreach ($array as $color): ?>
+						<li><a href = "#"><?=$color[1]?></a></li>
+					<? endforeach?>
+				</ul>
+			</div>
+		<? endif?>
+
+
+	</form>
 
 </div>
 <div class = "clear"></div>
@@ -128,10 +107,7 @@ $pics = CFile::ResizeImageGet($arResult['DETAIL_PICTURE']['ID'], array(
 	);
 	?>
 	<?foreach ($arResult['PROPERTIES'] as $key => $prop): ?>
-		<? if ($prop['VALUE'] != '' &&
-				!is_array($prop['VALUE']) &&
-				$prop['NAME'] != 'Базовая единица' &&
-				!in_array($key,$types)
+		<? if ($prop['VALUE'] != '' && !is_array($prop['VALUE']) && $prop['NAME'] != 'Базовая единица' && !in_array($key, $types)
 		): ?>
 			<tr>
 				<td width = "300"><b><?=$prop['NAME']?></b></td>
